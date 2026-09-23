d732545b4da280ed2983cc3a595ffb55df3ef74a
Sandesh-Ghimire0 <sandeshghimire1000@gmail.com>
2026-09-23 12:55:21 +0545

    serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.HandleFunc("/", home)               // subtree path
 	mux.HandleFunc("/snippet", showSnippet) // fixed path
 	mux.HandleFunc("/snippet/create", createSnippet)
 	// mux.HandleFunc("/snippet/create/", createSnippet) // subtree path --> if the url contains /snippet/create/** prefix then call createsnippet handler
+
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("starting the server on :4000")
 	err := http.ListenAndServe(":4000", mux)
